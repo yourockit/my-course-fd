59ec4229b688e4cf8cf194a9defbf967599caac5
Yourockit <sts2ro@gmail.com>
2023-06-21 15:17:07 +0300

    my-course-fd-projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -8,14 +8,14 @@ export const projects = [
     pageUrl: 'https://yourockit.github.io/my-course/my-html-css/my-hw-8/',
     sourceUrl:
       'https://github.com/yourockit/my-course/tree/master/my-html-css/my-hw-8',
-    title: 'HTML/CSS WebStudio',
+    title: 'HTML/CSS | WebStudio',
     description: 'Used HTMl, CSS, SASS. Mobile First. Adaptive layout.',
   },
   {
     imgUrl: imgGallery,
     pageUrl: 'https://yourockit.github.io/my-course-js-hw-11/',
     sourceUrl: 'https://github.com/yourockit/my-course-js-hw-11',
-    title: 'Vanilla JS Gallery',
+    title: 'Vanilla JS | Gallery',
     description:
       'Simple app for search images. Used Vanilla JS, Axios, REST API',
   },
@@ -23,7 +23,7 @@ export const projects = [
     imgUrl: imgPhonebook,
     pageUrl: 'https://yourockit.github.io/my-course-react-hw-08-phonebook/',
     sourceUrl: 'https://github.com/yourockit/my-course-react-hw-08-phonebook',
-    title: 'REACT Phonebook',
+    title: 'REACT | Phonebook',
     description:
       'App with the ability to store, create, delete and edit contacts. Used REACT, Redux, Redux Toolkit, Material UI, Axios and other libraries.',
   },
